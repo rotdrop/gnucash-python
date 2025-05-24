1d8dd4473f65f13056e570c039cb656cda6c9f5a
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-05-24 12:25:40 +0200

    Use gnucash_core_c with a hacked GnuCache version.

--- src/save-as.py
+++ src/save-as.py
@@ -5,10 +5,13 @@
 
 import argparse
 from urllib.parse import urlparse
+import gnucash
 from gnucash import (
     Session, GnuCashBackendException,
     SessionOpenMode,
-    ERR_BACKEND_LOCKED, ERR_FILEIO_FILE_NOT_FOUND
+    ERR_BACKEND_LOCKED,
+    ERR_FILEIO_FILE_NOT_FOUND,
+    gnucash_core_c,
 )
 
 parser = argparse.ArgumentParser(
@@ -22,6 +25,11 @@ parser.add_argument(
     'output',
     help='Output URI, e.g. mysql://user:password@host/database".',
 )
+parser.add_argument(
+    '--progress',
+    action='store_true',
+    help='Generate progress output.',
+)
 
 args = parser.parse_args()
 
@@ -31,14 +39,29 @@ outputParts = urlparse(args.output)
 print(inputParts)
 print(outputParts)
 
+if args.progress:
+    percentageFunc = gnucash_core_c.qof_percentage_func(lambda a, b: print(a, b) if a else print(b))
+else:
+    percentageFunc = None
+
 try:
-    inputSession = Session(args.input)
+    inputSession = gnucash_core_c.qof_session_new(gnucash_core_c.qof_book_new())
+    gnucash_core_c.qof_session_begin(inputSession, args.input, SessionOpenMode.SESSION_READ_ONLY)
+    gnucash_core_c.qof_session_load(inputSession, percentageFunc)
 except GnuCashBackendException as backend_exception:
     print(backend_exception)
     exit
 
 try:
-    outputSession = Session(args.output, SessionOpenMode.SESSION_NEW_OVERWRITE)
+    outputSession = gnucash_core_c.qof_session_new(gnucash_core_c.qof_book_new())
+    gnucash_core_c.qof_session_begin(outputSession, args.output, SessionOpenMode.SESSION_NEW_OVERWRITE)
+#    gnucash_core_c.qof_session_save(outputSession, None)
 except GnuCashBackendException as backend_exception:
     print(backend_exception)
     exit
+
+gnucash_core_c.qof_session_swap_data(inputSession, outputSession)
+gnucash_core_c.qof_book_mark_session_dirty(gnucash_core_c.qof_session_get_book(outputSession))
+
+gnucash_core_c.qof_session_save(outputSession, percentageFunc)
+gnucash_core_c.qof_session_end(outputSession)
